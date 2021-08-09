6d6bc19360c6b37212cbe88a3febeb2733ed2f85
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-08-09 19:41:04 +0200

    wip

--- api/posts/post-model.js
+++ api/posts/post-model.js
@@ -15,7 +15,7 @@ async function get() {
 
 async function getById(id) {
   // return db('posts').where({ id: id, foo: 'bar' })
-  return db('posts').where('id', id)
+  return db('posts').where('id', id).first()
 }
 
 async function create() {
